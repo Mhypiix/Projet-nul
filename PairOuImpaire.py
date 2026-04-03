326868501656c73598ef5f72e3e8558f33913ebd
Mhypiix <137407698+Mhypiix@users.noreply.github.com>
2026-04-03 13:28:59 +0200

    Add files via upload

---
+++ PairOuImpaire.py
@@ -0,0 +1,20 @@
+import tkinter as tk
+M = tk.Tk()
+label = tk.Label(M, text="", width=20)
+label.grid(row=0, column=0, columnspan=3)
+def verif():
+    try:
+        valeur = ENTRY.get()
+        if int(valeur)%2==0:
+            label.config(text="Pair")
+        else:
+            label.config(text="Impair")
+    except:
+        label.config(text="Ce n'est pas un nombre !")
+M.title("Page")
+M.geometry("1000x500")
+M.resizable(False, False)
+ENTRY = tk.Entry(font=("Arial", 24), width=50, justify = "center")
+ENTRY.grid(padx=30)
+Button1 = tk.Button(text="Entrée", bg="red", command=verif, width=40)
+Button1.grid(row=2, column=0, columnspan=3)
